e9ab0dfc39420fe7fe2f1acaad16a866d89a675f
小鸟游醋花 <2954683959@qq.com>
2025-01-25 18:14:05 +0800

    Delete 数模模拟赛1重构 directory

--- 数模模拟赛1重构/models/SimpleNN.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class SimpleNN(nn.Module):
-    def __init__(self, embedding_dim, num_classes):
-        super(SimpleNN, self).__init__()
-        self.fc = nn.Linear(embedding_dim, num_classes)
-    
-    def forward(self, x):
-        return self.fc(x)
--- 数模模拟赛1重构/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .my_cnn import MyCNN
-from .SimpleNN import SimpleNN
-from .newmy_cnn import OptimizedCNN
